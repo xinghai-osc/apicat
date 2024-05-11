b12f7b3914eb2076550cfae67101c18a223b8ab9
Yeying-Xingchen <yeyingxingchen@yeah.net>
2024-05-11 18:47:30 +0800

    新增 插件获取其相关配置。

--- apicat/config.py
+++ apicat/config.py
@@ -9,6 +9,13 @@ def get_port():
 def get_host():
     path = "./config.toml"
     if toml.load(path)['server']['host'] == None:
-        return 80
+        return "0.0.0.0"
+    else:
+        return toml.load(path)['server']['host']
+    
+def get_plugin_cfg(name,type,default=None):
+    path = "./config.toml"
+    if toml.load(path)[name][type] == None:
+        return default
     else:
         return toml.load(path)['server']['host']
