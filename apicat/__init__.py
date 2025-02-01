c9db581c8a7b0c97b65e34b0a36f9d18a9b46f2e
夜影星辰 <149694986+yeying-xingchen@users.noreply.github.com>
2025-02-01 18:59:20 +0800

    修改 插件API所在路径。

---
+++ apicat/__init__.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+import importlib
+from apicat import config
+app = FastAPI()
+
+cfg = config.read("config.toml")
+plugin_list = cfg["plugins"]
+
+for plugin_name in plugin_list:
+    plugin = importlib.import_module("apicat_"+plugin_name)
+    app.include_router(
+        plugin.router,
+        prefix="/api"+plugin.metadata.path,
+        tags=[plugin.metadata.tag]
+    )
+
+@app.get("/")
+def root_path():
+    return_message = {
+        "code": 200,
+        "message": "successes",
+        "plugin": "apicat_home",
+        "info": {
+            "message": "Welcome"
+        }
+    }
+    return return_message
