a1155dce73728035ae8464cb07e75971ef0bfcb9
夜影星辰 <149694986+yeying-xingchen@users.noreply.github.com>
2025-02-01 19:04:06 +0800

    修复：无插件类型的插件无法加载。

--- apicat/__init__.py
+++ apicat/__init__.py
@@ -8,18 +8,19 @@ plugin_list = cfg["plugins"]
 
 for plugin_name in plugin_list:
     plugin = importlib.import_module("apicat_"+plugin_name)
-    if plugin.metadata.type == "api" or plugin.metadata.type == "API" or plugin.metadata.type == "Api":
-        app.include_router(
-            plugin.router,
-            prefix="/api"+plugin.metadata.path,
-            tags=[plugin.metadata.tag]
-        )
-    elif plugin.metadata.type == "admin" or plugin.metadata.type == "Admin" or plugin.metadata.type == "ADMIN":
+    if plugin.metadata.type == "admin" or plugin.metadata.type == "Admin" or plugin.metadata.type == "ADMIN":
         app.include_router(
             plugin.router,
             prefix="/admin"+plugin.metadata.path,
             tags=[plugin.metadata.tag]
         )
+    else:
+        app.include_router(
+            plugin.router,
+            prefix="/api"+plugin.metadata.path,
+            tags=[plugin.metadata.tag]
+        )
+
 @app.get("/")
 def root_path():
     return_message = {
